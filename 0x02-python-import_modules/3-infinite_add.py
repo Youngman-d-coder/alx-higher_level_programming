aa85c2f2ed0b33147db63fcb8275ecd2fb11cb9a
Nelson <sayhitonelson@gmail.com>
2024-04-03 10:02:59 +0100

    update

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    args = argv[1:]
+    
+    if len(args) == 0:
+        print("0")
+    else:
+        result = sum(map(int, args))
+        print(result)
