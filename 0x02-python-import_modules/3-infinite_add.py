ec984c54424de50fafc7098e1edc1e9142e1eb02
Nelson <sayhitonelson@gmail.com>
2024-04-03 10:08:44 +0100

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from sys import argv
 
     args = argv[1:]
-    
+
     if len(args) == 0:
         print("0")
     else:
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.8
+import dis
+import types
+
+if __name__ == "__main__":
+    with open("hidden_4.pyc", "rb") as f:
+        code = f.read()
+    module = types.ModuleType("hidden_4")
+    exec(code, module.__dict__)
+
+    names = set()
+    for name, value in module.__dict__.items():
+        if not name.startswith("__"):
+            names.add(name)
+
+    for name in sorted(names):
+        print(name)
